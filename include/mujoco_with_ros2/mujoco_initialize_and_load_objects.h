288a94a6a7a19577030b1d92136c5a6286d1692c
Ashray4 <skspace12@gmail.com>
2025-09-11 19:37:18 +0200

    added floor and callbacks for moving during simulation

--- include/mujoco_with_ros2/mujoco_initialize_and_load_objects.h
+++ include/mujoco_with_ros2/mujoco_initialize_and_load_objects.h
@@ -10,6 +10,10 @@
  */
 //-----------------------------------------------------------------------------
 
+// The Purpose of this script is to load the objects,robots and other sensors into the simulation
+// and visualize them, for objects where a publisher or a ros2 controller is needed are initialized
+// and setup
+
 #pragma once
 
 #include <array>
@@ -42,6 +46,24 @@ public:
   MujocoInitLoadObjects(MujocoInitLoadObjects&&)                 = delete;
   MujocoInitLoadObjects& operator=(MujocoInitLoadObjects&&)      = delete;
 
+  // MuJoCo data structures
+  mjSpec* spec = NULL; // MuJoCo Spec
+  mjModel* m = NULL; // MuJoCo model
+  mjData* d  = NULL; // MuJoCo data
+  mjvCamera cam;     // abstract camera
+  mjvOption opt;     // visualization options
+  mjvScene scn;      // abstract scene
+  mjrContext con;    // custom GPU context
+
+
+  // mouse interaction
+  bool button_left   = false;
+  bool button_middle = false;
+  bool button_right  = false;
+  double lastx       = 0;
+  double lasty       = 0;
+
+
   //(To Review maybe a better way (Singleton Class))
   // static Init method to return an static instance of initialized simulation (static because
   // otherwise the method doesn't point from an object and is dangling, static so it can be called
@@ -49,6 +71,22 @@ public:
   static void init();
   // Initialize the Simulation and load objects
   void initialize_simulation();
+
+  // Keyboard callback
+  // static void keyboardCB(GLFWwindow* window, int key, int scancode, int act, int mods);
+  // void keyboardCBImpl(GLFWwindow* window, int key, int scancode, int act, int mods);
+
+  // Mouse button callback
+  static void mouseButtonCB(GLFWwindow* window, int button, int act, int mods);
+  void mouseButtonCBImpl(GLFWwindow* window, int button, int act, int mods);
+  //
+  //// Mouse move callback
+  static void mouseMoveCB(GLFWwindow* window, double xpos, double ypos);
+  void mouseMoveCBImpl(GLFWwindow* window, double xpos, double ypos);
+
+  // Scroll callback
+  static void scrollCB(GLFWwindow* window, double xoffset, double yoffset);
+  void scrollCBImpl(GLFWwindow* window, double xoffset, double yoffset);
 };
 
 
--- src/mujoco_initialize_and_load_objects.cpp
+++ src/mujoco_initialize_and_load_objects.cpp
@@ -22,6 +22,90 @@ namespace mujoco_with_ros2 {
 
 MujocoInitLoadObjects::MujocoInitLoadObjects() {}
 
+// mouse button callback
+void MujocoInitLoadObjects::mouseButtonCB(GLFWwindow* window, int button, int act, int mods)
+{
+  getInstance().mouseButtonCBImpl(window, button, act, mods);
+}
+
+void MujocoInitLoadObjects::mouseButtonCBImpl(GLFWwindow* window,
+                                              [[maybe_unused]] int button,
+                                              [[maybe_unused]] int act,
+                                              [[maybe_unused]] int mods)
+{
+  // update button state
+  button_left   = (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT) == GLFW_PRESS);
+  button_middle = (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_MIDDLE) == GLFW_PRESS);
+  button_right  = (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_RIGHT) == GLFW_PRESS);
+
+  // update mouse position
+  glfwGetCursorPos(window, &lastx, &lasty);
+}
+
+
+// mouse move callback
+void MujocoInitLoadObjects::mouseMoveCB(GLFWwindow* window, double xpos, double ypos)
+{
+  getInstance().mouseMoveCBImpl(window, xpos, ypos);
+}
+
+void MujocoInitLoadObjects::mouseMoveCBImpl(GLFWwindow* window, double xpos, double ypos)
+{
+  // no buttons down: nothing to do
+  if (!button_left && !button_middle && !button_right)
+  {
+    return;
+  }
+
+  // compute mouse displacement, save
+  double dx = xpos - lastx;
+  double dy = ypos - lasty;
+  lastx     = xpos;
+  lasty     = ypos;
+
+  // get current window size
+  int width, height;
+  glfwGetWindowSize(window, &width, &height);
+
+  // get shift key state
+  bool mod_shift = (glfwGetKey(window, GLFW_KEY_LEFT_SHIFT) == GLFW_PRESS ||
+                    glfwGetKey(window, GLFW_KEY_RIGHT_SHIFT) == GLFW_PRESS);
+
+  // determine action based on mouse button
+  mjtMouse action;
+  if (button_right)
+  {
+    action = mod_shift ? mjMOUSE_MOVE_H : mjMOUSE_MOVE_V;
+  }
+  else if (button_left)
+  {
+    action = mod_shift ? mjMOUSE_ROTATE_H : mjMOUSE_ROTATE_V;
+  }
+  else
+  {
+    action = mjMOUSE_ZOOM;
+  }
+
+  // move camera
+  mjv_moveCamera(m, action, dx / height, dy / height, &scn, &cam);
+}
+
+
+// scroll callback
+void MujocoInitLoadObjects::scrollCB(GLFWwindow* window, double xoffset, double yoffset)
+{
+  getInstance().scrollCBImpl(window, xoffset, yoffset);
+}
+
+void MujocoInitLoadObjects::scrollCBImpl([[maybe_unused]] GLFWwindow* window,
+                                         [[maybe_unused]] double xoffset,
+                                         double yoffset)
+{
+  // emulate vertical mouse motion = 5% of window height
+  mjv_moveCamera(m, mjMOUSE_ZOOM, 0, -0.05 * yoffset, &scn, &cam);
+}
+
+
 void MujocoInitLoadObjects::init()
 {
   std::cout << std::flush << "Simulation Initialized" << std::endl;
@@ -34,77 +118,103 @@ void MujocoInitLoadObjects::initialize_simulation()
   {
     char err_str[1000];
     int err_str_sz = 1000;
-    mjModel* m = mj_loadXML(
-      "/home/saksham/checkout/thesis_ws/colcon_ws/src/mujoco_with_ros2/models/ur5e/urdf/ur5e.xml",
+    spec           = mj_parseXML(
+      "/home/saksham/checkout/thesis_ws/colcon_ws/src/mujoco_with_ros2/models/ur5e/urdf/scene.xml",
       NULL,
       err_str,
       err_str_sz);
-    if (!m)
+    if (!spec)
     {
       std::cout << std::flush << "Problem with model" << std::endl;
       std::cout << std::flush << err_str << std::endl;
+      return;
     }
-
-    mjData* d = mj_makeData(m);
     
-    mjvCamera cam;                      // abstract camera
-    mjvOption opt;                      // visualization options
-    mjvScene scn;                       // abstract scene
-    mjrContext con;                     // custom GPU context
+    // std::cout << std::flush << "Looking for world frame" << std::endl;
+
+    // mjsBody* world = mjs_findBody(spec, "base");
+    // if (!world)
+    // {
+    //   std::cout << std::flush << "Problem with world frame (no world frame)" << std::endl;
+    //   return;
+    // }
+
+    // mjsGeom* world_floor = mjs_addGeom(world, NULL);
+    // world_floor->type    = mjGEOM_PLANE;
+    m                    = mj_compile(spec, NULL);
+    d                    = mj_makeData(m);
+
     if (!glfwInit())
     {
       mju_error("Could not initialize GLFW");
     }
-  GLFWwindow* window = glfwCreateWindow(1200, 900, "Demo", NULL, NULL);
-  glfwMakeContextCurrent(window);
-  glfwSwapInterval(1);
 
-  // initialize visualization data structures
-  mjv_defaultCamera(&cam);
-  mjv_defaultOption(&opt);
-  mjr_defaultContext(&con);
+    // create window, make OpenGL context current, request v-sync
+    GLFWwindow* window = glfwCreateWindow(1200, 900, "Demo", NULL, NULL);
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(1);
 
-  // create scene and context
-  mjv_makeScene(m, &scn, 1000);
-  mjr_makeContext(m, &con, mjFONTSCALE_100);
+    // initialize visualization data structures
+    mjv_defaultCamera(&cam);
+    mjv_defaultOption(&opt);
+    mjv_defaultScene(&scn);
+    mjr_defaultContext(&con);
 
-  // ... install GLFW keyboard and mouse callbacks
+    // create scene and context
+    mjv_makeScene(m, &scn, 2000);
+    mjr_makeContext(m, &con, mjFONTSCALE_150);
 
-  // run main loop, target real-time simulation and 60 fps rendering
-  while( !glfwWindowShouldClose(window) ) {
-    // advance interactive simulation for 1/60 sec
-    //  Assuming MuJoCo can simulate faster than real-time, which it usually can,
-    //  this loop will finish on time for the next frame to be rendered at 60 fps.
-    //  Otherwise add a cpu timer and exit this loop when it is time to render.
-    mjtNum simstart = d->time;
-    while( d->time - simstart < 1.0/60.0 )
+
+    // install GLFW mouse and keyboard callbacks
+    // glfwSetKeyCallback(window, keyboardCB);
+    glfwSetMouseButtonCallback(window, mouseButtonCB);
+    glfwSetCursorPosCallback(window, mouseMoveCB);
+    glfwSetScrollCallback(window, scrollCB);
+
+
+    // ... install GLFW keyboard and mouse callbacks
+
+    // run main loop, target real-time simulation and 60 fps rendering
+    while (!glfwWindowShouldClose(window))
+    {
+      // advance interactive simulation for 1/60 sec
+      //  Assuming MuJoCo can simulate faster than real-time, which it usually can,
+      //  this loop will finish on time for the next frame to be rendered at 60 fps.
+      //  Otherwise add a cpu timer and exit this loop when it is time to render.
+      mjtNum simstart = d->time;
+      while (d->time - simstart < 1.0 / 60.0)
+      {
         mj_step(m, d);
+      }
 
-    // get framebuffer viewport
-    mjrRect viewport = {0, 0, 0, 0};
-    glfwGetFramebufferSize(window, &viewport.width, &viewport.height);
+      // get framebuffer viewport
+      mjrRect viewport = {0, 0, 0, 0};
+      glfwGetFramebufferSize(window, &viewport.width, &viewport.height);
 
-    // update scene and render
-    mjv_updateScene(m, d, &opt, NULL, &cam, mjCAT_ALL, &scn);
-    mjr_render(viewport, &scn, &con);
+      // update scene and render
+      mjv_updateScene(m, d, &opt, NULL, &cam, mjCAT_ALL, &scn);
+      mjr_render(viewport, &scn, &con);
 
-    // swap OpenGL buffers (blocking call due to v-sync)
-    glfwSwapBuffers(window);
+      // swap OpenGL buffers (blocking call due to v-sync)
+      glfwSwapBuffers(window);
 
-    // process pending GUI events, call GLFW callbacks
-    glfwPollEvents();
-  }
+      // process pending GUI events, call GLFW callbacks
+      glfwPollEvents();
+    }
 
-  // close GLFW, free visualization storage
-  glfwTerminate();
-  mjv_freeScene(&scn);
-  mjr_freeContext(&con);
+    // free visualization storage
+    mjv_freeScene(&scn);
+    mjr_freeContext(&con);
+
+    // free MuJoCo model and data
+    mj_deleteData(d);
+    mj_deleteModel(m);
+    glfwTerminate();
   }
   catch (const std::exception& e)
   {
     std::cerr << e.what() << '\n';
   }
-
 };
 } // namespace mujoco_with_ros2
 
